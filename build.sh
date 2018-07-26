71a616760c7d5bc23d6856829e6c87f25dc3e18d
cixi <gengcx@spreadst.com>
2018-07-26 15:54:41 +0800

    on hikey board,the /dev/watchdog not exist, but also test.

--- build.sh
+++ build.sh
@@ -42,6 +42,7 @@ git am -s < ${TOPDIR}/patches/0001-st_filesystem_copy_file.c-when-has-O_CREAT-th
 git am -s < ${TOPDIR}/patches/0001-st_alsa_capture_file_test.c-when-has-O_CREAT-the-thi.patch
 git am -s < ${TOPDIR}/patches/0001-get_modular_config_names.sh-add-hikey-module-name.patch
 git am -s < ${TOPDIR}/patches/0001-common.sh-modify-LTPPATH.patch
+git am -s < ${TOPDIR}/patches/0001-get_devnode.sh-add-judgment-if-device-not-exist.patch
 
  
 cd $TOPDIR/ltp-ddt
